b472cd7bc0aa01d43c9611bae6a64d538337a5fa
Aiden <aiden@adsl-172-10-0-4.dsl.sndg02.sbcglobal.net>
2022-04-11 15:31:07 +0700

    test secret done

--- config/global.go
+++ config/global.go
@@ -11,13 +11,13 @@ import (
 type ENVConfig struct {
 	Port           int    `mapstructure:"PORT"`
 	Message        string `mapstructure:"MESSAGE"`
-	SecretMessage  string `mapstructure:"SECRET_MESSAGE"`
 	PrivateMessage string `mapstructure:"PRIVATE_MESSAGE"`
 }
 
 var EnvConfig ENVConfig
 
 func Init() error {
+	viper.AutomaticEnv()
 	fmt.Println(os.Environ())
 	_, path, _, _ := runtime.Caller(0)
 	fmt.Println(path)
@@ -25,7 +25,7 @@ func Init() error {
 	viper.AddConfigPath(fmt.Sprintf("%s/..", path))
 	viper.SetConfigName("app")
 	viper.SetConfigType("env")
-	viper.AutomaticEnv()
+	// viper.AutomaticEnv()
 	err := viper.ReadInConfig()
 	if err != nil {
 		return err
